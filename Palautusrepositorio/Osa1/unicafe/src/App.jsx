3a2b37254331222de922de2ef5468cadc69da879
Nummuliitti <papuusi@welho.com>
2025-04-10 16:44:39 +0300

    osa 1 unicafe tehtävä

--- Palautusrepositorio/Osa1/unicafe/src/App.jsx
+++ Palautusrepositorio/Osa1/unicafe/src/App.jsx
@@ -1,14 +1,47 @@
 import { useState } from 'react'
 
+
+const Button = ({ onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+
 const App = () => {
-  // tallenna napit omaan tilaansa
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+   }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+ }
+
   return (
     <div>
-      code here
+      <h3>Give feedback</h3>
+       
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      
+      <h3>Statistics</h3>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
